7ead17308e6482ac06d94ba17bf2a2aa1872b27e
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-08-19 21:30:06 +0300

    Lombok refactoring.

--- src/main/java/com/ambroziepaval/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/ambroziepaval/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.ambroziepaval.spring5recipeapp.services;
 
 import com.ambroziepaval.spring5recipeapp.domain.Recipe;
 import com.ambroziepaval.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the recipe service.");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().forEach(recipeSet::add);
         return recipeSet;
