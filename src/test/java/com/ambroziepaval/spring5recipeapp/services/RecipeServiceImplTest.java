a0b38a021f7d30b81770498e16a622dcf2920d74
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-09-01 15:58:08 +0300

    First JUnit & Mockito test.

---
+++ src/test/java/com/ambroziepaval/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.ambroziepaval.spring5recipeapp.services;
+
+import com.ambroziepaval.spring5recipeapp.domain.Recipe;
+import com.ambroziepaval.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
