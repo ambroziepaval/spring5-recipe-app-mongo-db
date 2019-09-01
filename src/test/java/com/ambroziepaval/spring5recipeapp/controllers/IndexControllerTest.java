963df20108da9af222222bbf6852345f86671f56
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-09-01 16:35:53 +0300

    Update Index Controller JUnit test.

--- src/test/java/com/ambroziepaval/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/ambroziepaval/spring5recipeapp/controllers/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package com.ambroziepaval.spring5recipeapp.controllers;
 
+import com.ambroziepaval.spring5recipeapp.domain.Recipe;
 import com.ambroziepaval.spring5recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -30,10 +35,25 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() {
 
-        String viewName = indexController.getIndexPage(model);
-        assertEquals("index", viewName);
+        // given
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
 
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipeSet.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // when
+        String viewName = indexController.getIndexPage(model);
+
+        // then
+        assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
