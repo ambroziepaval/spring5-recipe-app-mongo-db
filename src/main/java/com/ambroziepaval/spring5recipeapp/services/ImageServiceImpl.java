f001b171fb75d89f2040e6eced588863b0b19ca2
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-11-14 21:56:30 +0200

    Upload, Persist & Display images to/from DB w/ Spring MVC.

---
+++ src/main/java/com/ambroziepaval/spring5recipeapp/services/ImageServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ambroziepaval.spring5recipeapp.services;
+
+import com.ambroziepaval.spring5recipeapp.domain.Recipe;
+import com.ambroziepaval.spring5recipeapp.repositories.RecipeRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    @Override
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+        }
+    }
+}
